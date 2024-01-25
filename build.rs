06bad6c9d9875c3c64346486882b1b377e675d48
Leon Lux <leonlux@icloud.com>
2024-01-25 14:24:04 +0100

    update build script

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
         .compile(
-            &["proto/gnmi/gnmi.proto", "proto/gnmi_ext/gnmi_ext.proto", "proto/target/target.proto", "proto/collector/collector.proto"],
+            &["proto/gnmi.proto", "proto/gnmi_ext.proto", "proto/target.proto", "proto/collector.proto"],
             &["proto"]
         )?;
     Ok(())
