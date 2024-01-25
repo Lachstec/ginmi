3e02f161fa03f55b02b7518616c06f7aac640d79
Leon Lux <leonlux@icloud.com>
2024-01-25 15:37:05 +0100

    update build script to work with folders again

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
+        .compile_well_known_types(true)
         .compile(
-            &["proto/gnmi.proto", "proto/gnmi_ext.proto", "proto/target.proto", "proto/collector.proto"],
+            &["proto/gnmi/gnmi.proto", "proto/gnmi_ext/gnmi_ext.proto", "proto/target/target.proto", "proto/collector/collector.proto"],
             &["proto"]
         )?;
     Ok(())
