7701b4ee8562d1fe37fc0d60b970fadef13671d4
Leon Lux <leonlux@icloud.com>
2024-02-14 23:12:17 +0100

    add trait bounds to auth service in order to make it usable in GNmiClient

--- src/auth.rs
+++ src/auth.rs
@@ -1,49 +1,53 @@
+use std::error::Error;
+use http::{HeaderValue, Request};
 use std::sync::Arc;
 use std::task::{Context, Poll};
-use tonic::codegen::http::HeaderValue;
-use tonic::codegen::Service;
-use tonic::metadata::{Ascii, MetadataValue};
-use tonic::Request;
+use tonic::codegen::Body;
+use tower_service::Service;
 
 #[derive(Debug, Clone)]
-pub struct PasswordAuthService<S> {
+pub struct AuthService<S> {
     inner: S,
-    username: Option<Arc<MetadataValue<Ascii>>>,
-    password: Option<Arc<MetadataValue<Ascii>>>,
+    username: Option<Arc<HeaderValue>>,
+    password: Option<Arc<HeaderValue>>
 }
 
-impl<S> PasswordAuthService<S>  {
-    pub fn new(inner: S, username: Option<Arc<MetadataValue<Ascii>>>, password: Option<Arc<MetadataValue<Ascii>>>) -> Self {
-        Self {
-            inner,
-            username,
-            password
-        }
+impl<S> AuthService<S> {
+    #[inline]
+    pub fn new(inner: S, username: Option<Arc<HeaderValue>>, password: Option<Arc<HeaderValue>>) -> Self {
+        Self { inner, username, password }
     }
 }
 
-impl<S, Body, Response> Service<Request<Body>> for PasswordAuthService<S>
-where
-    S: Service<Request<Body>, Response = Response>
+impl<S, ReqBody, ResBody> Service<Request<ReqBody>> for AuthService<S>
+    where
+        S: Service<Request<ReqBody>, Response = ResBody>,
+        S::Error: ,
+        ResBody: Body,
+        <ResBody as Body>::Error: Into<Box<dyn Error + Send + Sync>>
+
 {
     type Response = S::Response;
     type Error = S::Error;
     type Future = S::Future;
 
+    #[inline]
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.inner.poll_ready(cx)
     }
 
-    fn call(&mut self, mut req: Request<Body>) -> Self::Future {
-        if let Some(username) = &self.username {
-            if let Some(password) = &self.password {
-                req.metadata_mut()
-                    .insert("username", username.as_ref().clone());
-                req.metadata_mut()
-                    .insert("password", password.as_ref().clone());
+    #[inline]
+    fn call(&mut self, mut request: Request<ReqBody>) -> Self::Future {
+        if let Some(user) = &self.username {
+            if let Some(pass) = &self.password {
+                request
+                    .headers_mut()
+                    .insert("username", user.as_ref().clone());
+                request.headers_mut()
+                    .insert("password", pass.as_ref().clone());
             }
         }
 
-        self.inner.call(req)
+        self.inner.call(request)
     }
 }
